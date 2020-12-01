39ccfc8d97ed18dad8b98067db805bd2b9bea97e
Gongmin Zhou <32424112+100607776@users.noreply.github.com>
2020-12-01 01:23:48 -0500

    Update Block.java

--- src/dsblockchain/Block.java
+++ src/dsblockchain/Block.java
@@ -2,5 +2,10 @@ package dsblockchain;
 import java.security.MessageDigest;
 
 public class Block {
+	public String hash;
+	public String previousHash;
+	private String data;
+	private String timestamp;
+	
 	
 }
