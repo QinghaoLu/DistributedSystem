3f564c0faf806df43f9370783250e364da7cce69
Weimin Zhao 100588860 <32662816+WeiminZhao@users.noreply.github.com>
2020-12-01 16:06:19 -0500

    update directory server

--- src/dsserver/directoryInterface.java
+++ src/dsserver/directoryInterface.java
@@ -4,7 +4,7 @@ import java.rmi.Remote;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 
-public interface directoryInterface {
+public interface directoryInterface extends Remote {
 
 	public boolean registerfunc(String name, String passwd,String ipaddr, String portNum) throws RemoteException;
 	
