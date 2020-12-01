835b9b7a5db040096f566e1681fb555c856fa0c0
Weimin Zhao 100588860 <32662816+WeiminZhao@users.noreply.github.com>
2020-11-30 22:05:48 -0500

    Create MainClientProgram.java

---
+++ src/dsmain/MainClientProgram.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package dsmain;
+
+/**
+ * @author zwmsc
+ *
+ */
+public class MainClientProgram {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
