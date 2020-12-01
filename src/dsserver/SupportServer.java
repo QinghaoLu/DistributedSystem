dfaced13c03815dc958c9f05e7781c47ee136be7
Weimin Zhao 100588860 <32662816+WeiminZhao@users.noreply.github.com>
2020-11-30 22:26:45 -0500

    Create SupportServer.java

---
+++ src/dsserver/SupportServer.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package dsserver;
+
+/**
+ * @author zwmsc
+ *
+ */
+public class SupportServer {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
