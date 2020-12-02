9a063f445c67891b81ee361463dcc27bb432fdd0
Qinghao Lu <100627854@oncampus.local>
2020-12-01 23:40:06 -0500

    Update RegisterClass.java

--- src/dsmain/RegisterClass.java
+++ src/dsmain/RegisterClass.java
@@ -12,22 +12,26 @@ public class RegisterClass {
 //		registration("hello");
 //	}
 
-	public static void registration(String portNum) {
+	public static UserInfo registration(String portNum) {
+
+		Scanner scanner = new Scanner(System.in);
 		try {
 			Registry regist = LocateRegistry.getRegistry();
 			directoryInterface fi = (directoryInterface) regist.lookup("directoryInterface");
 			String ipaddr;
 			ipaddr = InetAddress.getLocalHost().toString();
+			UserInfo userInfo;
 
-			System.out.println("Enter 1 to register, enter 2 to log in");
 			while (true) {
-				Scanner scanner = new Scanner(System.in);
+				System.out.println("Enter 1 to register, enter 2 to log in");
+
 				String check = scanner.nextLine();
 				if (check.equals("1")) {
 					System.out.println("Enter username: ");
 					String rUsername = scanner.nextLine();
 					System.out.println("Enter password: ");
 					String rPasswd = scanner.nextLine();
+
 					boolean rSucess = fi.registerfunc(rUsername, rPasswd, ipaddr, portNum);
 					if (rSucess) {
 						System.out.println("Regist success.");
@@ -35,7 +39,7 @@ public class RegisterClass {
 					} else {
 						System.out.println("Regist failed.");
 					}
-					
+
 				} else if (check.equals("2")) {
 					System.out.println("Enter username: ");
 					String lUsername = scanner.nextLine();
@@ -43,12 +47,14 @@ public class RegisterClass {
 					String lPasswd = scanner.nextLine();
 					boolean lSucess = fi.loginfunc(lUsername, lPasswd, ipaddr, portNum);
 					if (lSucess) {
+
 						System.out.println("Log in success.");
-						break;
+						userInfo = new UserInfo(lUsername, lPasswd, ipaddr, portNum);
+						return userInfo;
 					} else {
 						System.out.println("Log in failed.");
 					}
-		
+
 				} else {
 
 					System.out.println("Please enter 1 or 2.");
@@ -59,6 +65,9 @@ public class RegisterClass {
 		} catch (Exception e) {
 			System.out.println("Something going wrong. ");
 		}
+		scanner.close();
+		return null;
+
 	}
 
 }
