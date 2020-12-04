e779f6a5adb5ccaabf7444448798c4d6736ba9f0
zhoumu <zhou895230434@hotmail.com>
2020-12-03 21:22:42 -0500

    update

--- src/dsserver/SupportServer.java
+++ src/dsserver/SupportServer.java
@@ -17,6 +17,7 @@ public class SupportServer {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		System.setProperty("java.rmi.server.hostname", "192.168.0.31");
 		try{
 			
 			directoryInterface fi=new directoryImp("directoryServer");
